fca4377f9c472537b32d22ba2cda2c4a265dd8fd
GwonHyeok <kh4975@naver.com>
2014-03-03 11:43:07 +0000

    XuiMod: Toast Animations [2/2]

--- src/com/havoc/settings/fragments/Animations.java
+++ src/com/havoc/settings/fragments/Animations.java
@@ -15,29 +15,43 @@
  */
 package com.havoc.settings.fragments;
 
-import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ContentResolver; 
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.widget.Toast;
 
-import com.android.internal.logging.nano.MetricsProto; 
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
 
 import com.havoc.settings.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Animations extends SettingsPreferenceFragment  implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Animations";
     private static final String SCREEN_OFF_ANIMATION = "screen_off_animation"; 
+    private static final String KEY_TOAST_ANIMATION = "toast_animation";
 
     private ListPreference mScreenOffAnimation; 
+    private ListPreference mToastAnimation;
+
+    protected Context mContext;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -46,6 +60,8 @@ public class Animations extends SettingsPreferenceFragment  implements Preferenc
 
         addPreferencesFromResource(R.xml.havoc_settings_animations);
 
+        mContext = getActivity().getApplicationContext();
+
         // Screen Off Animations 
         mScreenOffAnimation = (ListPreference) findPreference(SCREEN_OFF_ANIMATION); 
         int screenOffStyle = Settings.System.getInt(resolver, 
@@ -53,6 +69,13 @@ public class Animations extends SettingsPreferenceFragment  implements Preferenc
         mScreenOffAnimation.setValue(String.valueOf(screenOffStyle)); 
         mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry()); 
         mScreenOffAnimation.setOnPreferenceChangeListener(this); 
+
+        mToastAnimation = (ListPreference) findPreference(KEY_TOAST_ANIMATION);
+        mToastAnimation.setSummary(mToastAnimation.getEntry());
+        int CurrentToastAnimation = Settings.System.getInt(getContentResolver(), Settings.System.TOAST_ANIMATION, 1);
+        mToastAnimation.setValueIndex(CurrentToastAnimation); //set to index of default value
+        mToastAnimation.setSummary(mToastAnimation.getEntries()[CurrentToastAnimation]);
+        mToastAnimation.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -64,7 +87,13 @@ public class Animations extends SettingsPreferenceFragment  implements Preferenc
             int valueIndex = mScreenOffAnimation.findIndexOfValue((String) newValue); 
             mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[valueIndex]); 
             return true; 
-        } 
+        }  else if (preference == mToastAnimation) {
+            int index = mToastAnimation.findIndexOfValue((String) objValue);
+            Settings.System.putString(getContentResolver(), Settings.System.TOAST_ANIMATION, (String) objValue);
+            mToastAnimation.setSummary(mToastAnimation.getEntries()[index]);
+            Toast.makeText(mContext, "Toast Test", Toast.LENGTH_SHORT).show();
+            return true;
+        }
         return false; 
     }
 
