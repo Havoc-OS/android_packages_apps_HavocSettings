114043983be4f46343a0d893abe22141f6d75652
beanstown106 <nbenis106@gmail.com>
2017-05-11 19:36:44 +0800

    Incall vibration options [3/3]
    *allow setting vibration when call is connected
    *allow setting vibration when call is disconnected
    *allow setting vibration for call waiting
    *this works with google and aosp dialer :)

--- src/com/havoc/settings/fragments/Notifications.java
+++ src/com/havoc/settings/fragments/Notifications.java
@@ -13,34 +13,70 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.havoc.settings.fragments;
-
-import android.app.Activity;
+package com.havoc.settings.fragments; 
+ 
+import android.app.ActivityManagerNative;
 import android.content.Context;
-import android.content.Intent;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v7.preference.ListPreference;
+import android.os.UserHandle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import android.util.Log;
+import android.view.WindowManagerGlobal;
+import android.view.IWindowManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.development.DevelopmentSettings;
+import java.util.Locale;
+import android.text.TextUtils;
+import android.view.View;
+
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
 
-import com.havoc.settings.R;
+import com.havoc.settings.preferences.Utils;
 
-public class Notifications extends SettingsPreferenceFragment {
-
+public class Notifications extends SettingsPreferenceFragment
+    implements OnPreferenceChangeListener {
+ 
     public static final String TAG = "Notifications";
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.havoc_settings_notifications);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefSet = getPreferenceScreen();
+		
+		PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS); 
+        if (!Utils.isVoiceCapable(getActivity())) {
+        prefSet.removePreference(incallVibCategory); 
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
     }
 
     @Override
