6e71108278895c945e8198bf2784560fcca530fe
cristianomatos <cristianobmatos@gmail.com>
2017-09-05 13:26:07 +0800

    RecentSettings: recents panel clear all button tweaks * Show or hide recents button * Place the button in top left, center, right or bottom left, center, right

--- src/com/havoc/settings/fragments/Recents.java
+++ src/com/havoc/settings/fragments/Recents.java
@@ -15,31 +15,64 @@
  */
 package com.havoc.settings.fragments;
 
+import com.android.internal.logging.nano.MetricsProto;
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.content.ContentResolver;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
+import com.android.settings.R;
 
-import com.android.internal.logging.nano.MetricsProto; 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import com.android.settings.SettingsPreferenceFragment;
 
-import com.havoc.settings.R;
+public class Recents extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
 
-public class Recents extends SettingsPreferenceFragment {
-
-    public static final String TAG = "Recents";
+    private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+    private ListPreference mRecentsClearAllLocation;
+    private SwitchPreference mRecentsClearAll;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.havoc_settings_recents);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        // clear all recents
+        mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
+        int location = Settings.System.getIntForUser(resolver,
+                Settings.System.RECENTS_CLEAR_ALL_LOCATION, 3, UserHandle.USER_CURRENT);
+        mRecentsClearAllLocation.setValue(String.valueOf(location));
+        mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntry());
+        mRecentsClearAllLocation.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mRecentsClearAllLocation) {
+            int location = Integer.valueOf((String) objValue);
+            int index = mRecentsClearAllLocation.findIndexOfValue((String) objValue);
+            Settings.System.putIntForUser(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_CLEAR_ALL_LOCATION, location, UserHandle.USER_CURRENT);
+            mRecentsClearAllLocation.setSummary(mRecentsClearAllLocation.getEntries()[index]);
+        return true;
+        }
+    return false;
+
     }
 
     @Override
