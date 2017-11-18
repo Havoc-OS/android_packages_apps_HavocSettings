54a1527f3f04487154d4e4cac3905c175093241f
xyyx <xyyx@mail.ru>
2017-11-18 23:24:47 +0800

    Add Oneplus3Parts to main menu

--- src/com/havoc/settings/HavocSettings.java
+++ src/com/havoc/settings/HavocSettings.java
@@ -26,6 +26,7 @@ import android.os.Bundle;
 import android.view.Surface;
 import android.preference.Preference;
 import com.android.settings.R;
+import com.havoc.settings.preferences.Utils;
 
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -34,8 +35,15 @@ public class HavocSettings extends SettingsPreferenceFragment {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        final String KEY_DEVICE_PART = "device_part";
+        final String KEY_DEVICE_PART_PACKAGE_NAME = "org.omnirom.device";
 
         addPreferencesFromResource(R.xml.havoc_settings);
+
+        // DeviceParts
+        if (!Utils.isPackageInstalled(getActivity(), KEY_DEVICE_PART_PACKAGE_NAME)) {
+            getPreferenceScreen().removePreference(findPreference(KEY_DEVICE_PART));
+        }
     }
 
     @Override
