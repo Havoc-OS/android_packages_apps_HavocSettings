bd7067b351367f53964d136d26b955a4dadfb258
bigrushdog <randall.rushing@gmail.com>
2018-01-23 10:22:20 -0800

    Screen off animations [2/2]

--- src/com/havoc/settings/fragments/Animations.java
+++ src/com/havoc/settings/fragments/Animations.java
@@ -18,6 +18,7 @@ package com.havoc.settings.fragments;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ContentResolver; 
 import android.os.Bundle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
@@ -31,15 +32,40 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import com.havoc.settings.R;
 
-public class Animations extends SettingsPreferenceFragment {
+public class Animations extends SettingsPreferenceFragment  implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Animations";
+    private static final String SCREEN_OFF_ANIMATION = "screen_off_animation"; 
+
+    private ListPreference mScreenOffAnimation; 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver(); 
 
         addPreferencesFromResource(R.xml.havoc_settings_animations);
+
+        // Screen Off Animations 
+        mScreenOffAnimation = (ListPreference) findPreference(SCREEN_OFF_ANIMATION); 
+        int screenOffStyle = Settings.System.getInt(resolver, 
+                 Settings.System.SCREEN_OFF_ANIMATION, 0); 
+        mScreenOffAnimation.setValue(String.valueOf(screenOffStyle)); 
+        mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry()); 
+        mScreenOffAnimation.setOnPreferenceChangeListener(this); 
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver(); 
+        if (preference == mScreenOffAnimation) { 
+            Settings.System.putInt(getContentResolver(), 
+                    Settings.System.SCREEN_OFF_ANIMATION, Integer.valueOf((String) newValue)); 
+            int valueIndex = mScreenOffAnimation.findIndexOfValue((String) newValue); 
+            mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[valueIndex]); 
+            return true; 
+        } 
+        return false; 
     }
 
     @Override
