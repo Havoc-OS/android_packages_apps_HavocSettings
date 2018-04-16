474d16a2960516351a698855cc7c1699062fe59d
bigrushdog <randall.rushing@gmail.com>
2018-04-16 13:46:47 +0530

    Add Font preference for font manager

--- src/com/havoc/settings/fragments/Interface.java
+++ src/com/havoc/settings/fragments/Interface.java
@@ -15,33 +15,74 @@
  */
 package com.havoc.settings.fragments;
 
-import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
-import android.content.Intent;
+import android.content.FontInfo;
+import android.content.IFontService;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
-import android.support.v14.preference.SwitchPreference;
-import android.provider.Settings;
+import android.widget.Toast;
 
-import com.android.internal.logging.nano.MetricsProto;
-import com.android.settings.development.DevelopmentSettings;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.internal.logging.nano.MetricsProto;
 
+import com.havoc.settings.fragments.ui.FontDialogPreference;
 import com.havoc.settings.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Interface extends SettingsPreferenceFragment {
 
-    public static final String TAG = "Interface";
+    private static final String KEY_FONT_PICKER_FRAGMENT_PREF = "custom_font";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
+    private FontDialogPreference mFontPreference;
+    private IFontService mFontService;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         addPreferencesFromResource(R.xml.havoc_settings_interface);
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+       mFontPreference =  (FontDialogPreference) findPreference(KEY_FONT_PICKER_FRAGMENT_PREF);
+       mFontService = IFontService.Stub.asInterface(
+                ServiceManager.getService("havocfont"));
+
+        if (!isPackageInstalled(SUBS_PACKAGE, getActivity())) {
+            mFontPreference.setSummary(getCurrentFontInfo().fontName.replace("_", " "));
+        } else {
+            mFontPreference.setSummary(getActivity().getString(
+                    R.string.disable_fonts_installed_title));
+        }
     }
+
+    private FontInfo getCurrentFontInfo() {
+        try {
+            return mFontService.getFontInfo();
+        } catch (RemoteException e) {
+            return FontInfo.getDefaultFontInfo();
+        }
+    }
+
+    private boolean isPackageInstalled(String package_name, Context context) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(package_name, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }	
 
     @Override
     public int getMetricsCategory() {
