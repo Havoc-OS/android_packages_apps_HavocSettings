26645d4c462caf95f5d02996680deec5f2175728
Alex Cruz <mazdarider23@gmail.com>
2018-05-22 11:51:26 +0530

    Settings: Increase gesture anywhere width to 40px [1/2]

--- src/com/havoc/settings/fragments/GestureAnywhereSettings.java
+++ src/com/havoc/settings/fragments/GestureAnywhereSettings.java
@@ -67,7 +67,7 @@ public class GestureAnywhereSettings extends SettingsPreferenceFragment implemen
 
         mTriggerWidthPref = (CustomSeekBarPreference) findPreference(KEY_TRIGGER_WIDTH);
         mTriggerWidthPref.setValue(Settings.System.getInt(getContentResolver(),
-                Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 10));
+                Settings.System.GESTURE_ANYWHERE_TRIGGER_WIDTH, 40));
         mTriggerWidthPref.setOnPreferenceChangeListener(this);
 
         mTriggerTopPref = (CustomSeekBarPreference) findPreference(KEY_TRIGGER_TOP);
