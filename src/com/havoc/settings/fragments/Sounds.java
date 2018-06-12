75e3ba83957337c84c53a669d3a5b37357562166
ezio84 <brabus84@gmail.com>
2018-06-12 12:07:53 +0200

    BT Headset music launch actions [2/2]

--- src/com/havoc/settings/fragments/Sounds.java
+++ src/com/havoc/settings/fragments/Sounds.java
@@ -43,7 +43,10 @@ Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Sound";
 
+    private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player"; 
+
     private ListPreference mNoisyNotification;
+    private ListPreference mLaunchPlayerHeadsetConnection;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,7 +63,14 @@ Preference.OnPreferenceChangeListener {
         mNoisyNotification.setValue(String.valueOf(mode));
         mNoisyNotification.setSummary(mNoisyNotification.getEntry());
         mNoisyNotification.setOnPreferenceChangeListener(this);
-    }
+
+        mLaunchPlayerHeadsetConnection = (ListPreference) findPreference(HEADSET_CONNECT_PLAYER); 
+        int mLaunchPlayerHeadsetConnectionValue = Settings.System.getIntForUser(resolver, 
+                Settings.System.HEADSET_CONNECT_PLAYER, 0, UserHandle.USER_CURRENT); 
+        mLaunchPlayerHeadsetConnection.setValue(Integer.toString(mLaunchPlayerHeadsetConnectionValue)); 
+        mLaunchPlayerHeadsetConnection.setSummary(mLaunchPlayerHeadsetConnection.getEntry()); 
+        mLaunchPlayerHeadsetConnection.setOnPreferenceChangeListener(this); 
+    } 
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -73,6 +83,14 @@ Preference.OnPreferenceChangeListener {
             mNoisyNotification.setSummary(
                     mNoisyNotification.getEntries()[index]);
             return true;
+        } else if (preference == mLaunchPlayerHeadsetConnection) { 
+            int mLaunchPlayerHeadsetConnectionValue = Integer.valueOf((String) newValue); 
+            int index = mLaunchPlayerHeadsetConnection.findIndexOfValue((String) newValue); 
+            mLaunchPlayerHeadsetConnection.setSummary( 
+                    mLaunchPlayerHeadsetConnection.getEntries()[index]); 
+            Settings.System.putIntForUser(resolver, Settings.System.HEADSET_CONNECT_PLAYER, 
+                    mLaunchPlayerHeadsetConnectionValue, UserHandle.USER_CURRENT); 
+            return true; 
         }
         return false;
     }
