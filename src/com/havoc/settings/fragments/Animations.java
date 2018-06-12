bdfded00423c43a12e397b29633e058ada0706c5
Squadzone <ai.the.smarties.physics@gmail.com>
2018-06-12 15:04:05 +0200

    Custom scrolling values [2/2]

--- src/com/havoc/settings/fragments/Animations.java
+++ src/com/havoc/settings/fragments/Animations.java
@@ -33,10 +33,21 @@ import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference; 
 import android.widget.Toast;
+import android.view.ViewConfiguration; 
+import android.view.Menu; 
+import android.view.MenuInflater; 
+import android.view.MenuItem; 
+import android.util.Log; 
+import android.text.TextUtils; 
+import android.content.DialogInterface; 
+import android.app.AlertDialog; 
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
+
+import com.havoc.settings.preferences.CustomSeekBarPreference; 
 
 import com.havoc.settings.fragments.ui.AnimationControls;
 import com.havoc.settings.fragments.ui.DozeFragment;
@@ -63,7 +74,22 @@ public class Animations extends SettingsPreferenceFragment
     private static final String SCROLLINGCACHE_PREF = "pref_scrollingcache";
     private static final String SCROLLINGCACHE_PERSIST_PROP = "persist.sys.scrollingcache";
     private static final String SCROLLINGCACHE_DEFAULT = "1";
-
+    private static final String TAG = "ScrollAnimationInterfaceSettings"; 
+ 
+    private static final String ANIMATION_FLING_VELOCITY = "animation_fling_velocity"; 
+    private static final String ANIMATION_SCROLL_FRICTION = "animation_scroll_friction"; 
+    private static final String ANIMATION_OVERSCROLL_DISTANCE = "animation_overscroll_distance"; 
+    private static final String ANIMATION_OVERFLING_DISTANCE = "animation_overfling_distance"; 
+    private static final float MULTIPLIER_SCROLL_FRICTION = 10000f; 
+    private static final String ANIMATION_NO_SCROLL = "animation_no_scroll"; 
+ 
+    private static final int MENU_RESET = Menu.FIRST; 
+ 
+    private CustomSeekBarPreference mAnimationFling; 
+    private CustomSeekBarPreference mAnimationScroll; 
+    private CustomSeekBarPreference mAnimationOverScroll; 
+    private CustomSeekBarPreference mAnimationOverFling; 
+    private SwitchPreference mAnimNoScroll; 
     private ListPreference mToastAnimation;
     private ListPreference mListViewAnimation;
     private ListPreference mListViewInterpolator;
@@ -156,7 +182,87 @@ public class Animations extends SettingsPreferenceFragment
             mToast = null;
         }
 
+        mAnimNoScroll = (SwitchPreference) prefScreen.findPreference(ANIMATION_NO_SCROLL); 
+        mAnimNoScroll.setChecked(Settings.System.getInt(resolver, 
+                Settings.System.ANIMATION_CONTROLS_NO_SCROLL, 0) == 1); 
+        mAnimNoScroll.setOnPreferenceChangeListener(this); 
+ 
+        float defaultScroll = Settings.System.getFloat(resolver, 
+                Settings.System.CUSTOM_SCROLL_FRICTION, ViewConfiguration.DEFAULT_SCROLL_FRICTION); 
+        mAnimationScroll = (CustomSeekBarPreference) prefScreen.findPreference(ANIMATION_SCROLL_FRICTION); 
+        mAnimationScroll.setValue((int) (defaultScroll * MULTIPLIER_SCROLL_FRICTION)); 
+        mAnimationScroll.setOnPreferenceChangeListener(this); 
+ 
+        int defaultFling = Settings.System.getInt(resolver, 
+                Settings.System.CUSTOM_FLING_VELOCITY, ViewConfiguration.DEFAULT_MAXIMUM_FLING_VELOCITY); 
+        mAnimationFling = (CustomSeekBarPreference) prefScreen.findPreference(ANIMATION_FLING_VELOCITY); 
+        mAnimationFling.setValue(defaultFling); 
+        mAnimationFling.setOnPreferenceChangeListener(this); 
+ 
+        int defaultOverScroll = Settings.System.getInt(resolver, 
+                Settings.System.CUSTOM_OVERSCROLL_DISTANCE, ViewConfiguration.DEFAULT_OVERSCROLL_DISTANCE); 
+        mAnimationOverScroll = (CustomSeekBarPreference) prefScreen.findPreference(ANIMATION_OVERSCROLL_DISTANCE); 
+        mAnimationOverScroll.setValue(defaultOverScroll); 
+        mAnimationOverScroll.setOnPreferenceChangeListener(this); 
+ 
+        int defaultOverFling = Settings.System.getInt(resolver, 
+                Settings.System.CUSTOM_OVERFLING_DISTANCE, ViewConfiguration.DEFAULT_OVERFLING_DISTANCE); 
+        mAnimationOverFling = (CustomSeekBarPreference) prefScreen.findPreference(ANIMATION_OVERFLING_DISTANCE); 
+        mAnimationOverFling.setValue(defaultOverFling); 
+        mAnimationOverFling.setOnPreferenceChangeListener(this); 
+ 
+        setHasOptionsMenu(true); 
+
     }
+
+    @Override 
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) { 
+        menu.add(0, MENU_RESET, 0, R.string.reset) 
+                .setIcon(R.drawable.ic_settings_backup) // use the backup icon 
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS); 
+    } 
+ 
+    @Override 
+    public boolean onOptionsItemSelected(MenuItem item) { 
+        switch (item.getItemId()) { 
+            case MENU_RESET: 
+                resetToDefault(); 
+                return true; 
+            default: 
+                return super.onContextItemSelected(item); 
+        } 
+    } 
+ 
+    private void resetToDefault() { 
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity()); 
+        alertDialog.setTitle(R.string.reset); 
+        alertDialog.setMessage(R.string.animation_settings_reset_message); 
+        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() { 
+            public void onClick(DialogInterface dialog, int id) { 
+                resetAllValues(); 
+                resetAllSettings(); 
+            } 
+        }); 
+        alertDialog.setNegativeButton(R.string.cancel, null); 
+        alertDialog.create().show(); 
+    } 
+ 
+    private void resetAllValues() { 
+        mAnimationFling.setValue(ViewConfiguration.DEFAULT_MAXIMUM_FLING_VELOCITY); 
+        mAnimationScroll.setValue((int) (ViewConfiguration.DEFAULT_SCROLL_FRICTION * MULTIPLIER_SCROLL_FRICTION)); 
+        mAnimationOverScroll.setValue(ViewConfiguration.DEFAULT_OVERSCROLL_DISTANCE); 
+        mAnimationOverFling.setValue(ViewConfiguration.DEFAULT_OVERFLING_DISTANCE); 
+        mAnimNoScroll.setChecked(false); 
+    } 
+ 
+    private void resetAllSettings() { 
+        setProperVal(mAnimationFling, ViewConfiguration.DEFAULT_MAXIMUM_FLING_VELOCITY); 
+        Settings.System.putFloat(getActivity().getContentResolver(), 
+                   Settings.System.CUSTOM_SCROLL_FRICTION, ViewConfiguration.DEFAULT_SCROLL_FRICTION); 
+        setProperVal(mAnimationOverScroll, ViewConfiguration.DEFAULT_OVERSCROLL_DISTANCE); 
+        setProperVal(mAnimationOverFling, ViewConfiguration.DEFAULT_OVERFLING_DISTANCE); 
+        setProperVal(mAnimNoScroll, 0); 
+    } 
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -227,9 +333,47 @@ public class Animations extends SettingsPreferenceFragment
             SystemProperties.set(SCROLLINGCACHE_PERSIST_PROP, value);
             mScrollingCachePref.setSummary(mScrollingCachePref.getEntries()[index]);
             return true;
-        }
+        } else if (preference == mAnimNoScroll) { 
+                boolean value = (Boolean) newValue; 
+                Settings.System.putInt(resolver, Settings.System.ANIMATION_CONTROLS_NO_SCROLL, value ? 1 : 0); 
+            } else if (preference == mAnimationScroll) { 
+                int val = ((Integer)newValue).intValue(); 
+                Settings.System.putFloat(resolver, 
+                       Settings.System.CUSTOM_SCROLL_FRICTION, 
+                       ((float) (val / MULTIPLIER_SCROLL_FRICTION))); 
+            } else if (preference == mAnimationFling) { 
+                int val = ((Integer)newValue).intValue(); 
+                Settings.System.putInt(resolver, 
+                        Settings.System.CUSTOM_FLING_VELOCITY, 
+                        val); 
+            } else if (preference == mAnimationOverScroll) { 
+                int val = ((Integer)newValue).intValue(); 
+                Settings.System.putInt(resolver, 
+                        Settings.System.CUSTOM_OVERSCROLL_DISTANCE, 
+                        val); 
+            } else if (preference == mAnimationOverFling) { 
+                int val = ((Integer)newValue).intValue(); 
+                Settings.System.putInt(resolver, 
+                        Settings.System.CUSTOM_OVERFLING_DISTANCE, 
+                        val); 
+            }
         return false;
     }
+
+    private void setProperVal(Preference preference, int val) { 
+        String mString = ""; 
+        if (preference == mAnimNoScroll) { 
+            mString = Settings.System.ANIMATION_CONTROLS_NO_SCROLL; 
+        } else if (preference == mAnimationFling) { 
+            mString = Settings.System.CUSTOM_FLING_VELOCITY; 
+        } else if (preference == mAnimationOverScroll) { 
+            mString = Settings.System.CUSTOM_OVERSCROLL_DISTANCE; 
+        } else if (preference == mAnimationOverFling) { 
+            mString = Settings.System.CUSTOM_OVERFLING_DISTANCE; 
+        } 
+ 
+        Settings.System.putInt(getActivity().getContentResolver(), mString, val); 
+    } 
 
     public static void reset(Context mContext) {
         ContentResolver resolver = mContext.getContentResolver();
