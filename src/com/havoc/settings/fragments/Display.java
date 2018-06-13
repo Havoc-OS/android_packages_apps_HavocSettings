cb000169c1b6140517ab4f795ed8850c5f1f13c2
brituj <ritujbeniwal@gmail.com>
2018-06-13 09:15:14 +0200

    Add Screen Stabilization

--- src/com/havoc/settings/fragments/Display.java
+++ src/com/havoc/settings/fragments/Display.java
@@ -21,14 +21,12 @@ import android.content.res.Resources;
 import android.os.Bundle; 
 import android.os.UserHandle; 
 import android.os.PowerManager; 
-import android.preference.ListPreference; 
-import android.preference.SwitchPreference; 
-import android.preference.Preference; 
-import android.preference.PreferenceCategory; 
-import android.preference.PreferenceManager; 
-import android.preference.PreferenceScreen; 
-import android.preference.Preference.OnPreferenceChangeListener; 
-import android.provider.Settings; 
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
  
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.R; 
@@ -36,13 +34,16 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils; 
 import com.havoc.settings.preferences.SystemSettingSwitchPreference; 
  
-public class SmartPixels extends SettingsPreferenceFragment implements 
+public class Display extends SettingsPreferenceFragment implements 
         Preference.OnPreferenceChangeListener { 
-    private static final String TAG = "SmartPixels"; 
+    private static final String TAG = "Display"; 
  
     private static final String ON_POWER_SAVE = "smart_pixels_on_power_save"; 
  
     private SystemSettingSwitchPreference mSmartPixelsOnPowerSave; 
+    private ListPreference mVelocityFriction;
+    private ListPreference mPositionFriction;
+    private ListPreference mVelocityAmplitude;
  
     ContentResolver resolver; 
  
@@ -59,6 +60,33 @@ public class SmartPixels extends SettingsPreferenceFragment implements
         updateDependency(); 
  
         mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.smart_pixels_footer); 
+
+        float velFriction = Settings.System.getFloatForUser(resolver,
+                    Settings.System.STABILIZATION_VELOCITY_FRICTION,
+                    0.1f,
+                    UserHandle.USER_CURRENT);
+        mVelocityFriction = (ListPreference) findPreference("stabilization_velocity_friction");
+    	mVelocityFriction.setValue(Float.toString(velFriction));
+    	mVelocityFriction.setSummary(mVelocityFriction.getEntry());
+    	mVelocityFriction.setOnPreferenceChangeListener(this);
+    	
+    	float posFriction = Settings.System.getFloatForUser(resolver,
+                    Settings.System.STABILIZATION_POSITION_FRICTION,
+                    0.1f,
+                    UserHandle.USER_CURRENT);
+            mPositionFriction = (ListPreference) findPreference("stabilization_position_friction");
+    	mPositionFriction.setValue(Float.toString(posFriction));
+    	mPositionFriction.setSummary(mPositionFriction.getEntry());
+    	mPositionFriction.setOnPreferenceChangeListener(this);
+    
+    	int velAmplitude = Settings.System.getIntForUser(resolver,
+                    Settings.System.STABILIZATION_VELOCITY_AMPLITUDE,
+                    8000,
+                    UserHandle.USER_CURRENT);
+            mVelocityAmplitude = (ListPreference) findPreference("stabilization_velocity_amplitude");
+    	mVelocityAmplitude.setValue(Integer.toString(velAmplitude));
+    	mVelocityAmplitude.setSummary(mVelocityAmplitude.getEntry());
+    	mVelocityAmplitude.setOnPreferenceChangeListener(this);
     } 
  
     @Override
@@ -78,9 +106,39 @@ public class SmartPixels extends SettingsPreferenceFragment implements
  
     public boolean onPreferenceChange(Preference preference, Object objValue) { 
         final String key = preference.getKey(); 
+        final String setting = getSystemPreferenceString(preference);
+        if (preference != null && preference instanceof ListPreference) {
+               ListPreference listPref = (ListPreference) preference;
+               String value = (String) objValue;
+               int index = listPref.findIndexOfValue(value);
+               listPref.setSummary(listPref.getEntries()[index]);
+    	    listPref.setValue(value);
+    	    if(preference != mVelocityAmplitude){
+    		Settings.System.putFloatForUser(getContentResolver(), setting, Float.valueOf(value),
+    			UserHandle.USER_CURRENT);
+    	    }else {
+    		Settings.System.putIntForUser(getContentResolver(), setting, Integer.valueOf(value),
+    			UserHandle.USER_CURRENT);
+    	    }
+    	}
         updateDependency(); 
+
         return true; 
     } 
+
+    private String getSystemPreferenceString(Preference preference) {
+      if (preference == null) {
+                 return "";
+      } else if(preference == mVelocityFriction){
+          return Settings.System.STABILIZATION_VELOCITY_FRICTION;
+      } else if(preference == mPositionFriction){
+          return Settings.System.STABILIZATION_POSITION_FRICTION;
+      } else if(preference == mVelocityAmplitude){
+          return Settings.System.STABILIZATION_VELOCITY_AMPLITUDE;
+      }
+      
+      return "";
+    }
  
     private void updateDependency() { 
         boolean mUseOnPowerSave = (Settings.System.getIntForUser( 
