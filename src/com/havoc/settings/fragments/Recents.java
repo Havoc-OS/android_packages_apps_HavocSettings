8a334bd39099acff059d86379c5f5c977edc81a5
Alex Cruz <alex@dirtyunicorns.com>
2018-10-13 16:38:00 +0530

    Immersive Recents [2/2]

--- src/com/havoc/settings/fragments/Recents.java
+++ src/com/havoc/settings/fragments/Recents.java
@@ -26,6 +26,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.content.SharedPreferences; 
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
@@ -49,10 +50,16 @@ public class Recents extends SettingsPreferenceFragment implements
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
     private static final String NAVIGATION_BAR_RECENTS_STYLE = "navbar_recents_style";
     private static final String RECENTS_COMPONENT_TYPE = "recents_component";
+    private static final String IMMERSIVE_RECENTS = "immersive_recents"; 
+
+    private ListPreference mImmersiveRecents; 
     private ListPreference mRecentsClearAllLocation;
     private SwitchPreference mRecentsClearAll;
     private ListPreference mNavbarRecentsStyle;
     private ListPreference mRecentsComponentType;
+
+    private SharedPreferences mPreferences; 
+    private Context mContext; 
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -61,6 +68,7 @@ public class Recents extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.havoc_settings_recents);
 
         ContentResolver resolver = getActivity().getContentResolver();
+        mContext = getActivity().getApplicationContext(); 
 
         // clear all recents
         mRecentsClearAllLocation = (ListPreference) findPreference(RECENTS_CLEAR_ALL_LOCATION);
@@ -84,6 +92,13 @@ public class Recents extends SettingsPreferenceFragment implements
         mRecentsComponentType.setValue(String.valueOf(type));
         mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
         mRecentsComponentType.setOnPreferenceChangeListener(this);
+
+        mImmersiveRecents = (ListPreference) findPreference(IMMERSIVE_RECENTS); 
+        mImmersiveRecents.setValue(String.valueOf(Settings.System.getIntForUser( 
+                resolver, Settings.System.IMMERSIVE_RECENTS, 0, UserHandle.USER_CURRENT))); 
+        mImmersiveRecents.setSummary(mImmersiveRecents.getEntry()); 
+        mImmersiveRecents.setOnPreferenceChangeListener(this); 
+
     }
 
     @Override
@@ -125,6 +140,21 @@ public class Recents extends SettingsPreferenceFragment implements
             }
             HavocUtils.showSystemUiRestartDialog(getContext());
             return true;
+        }  if (preference == mImmersiveRecents) {
+            Settings.System.putIntForUser(resolver, Settings.System.IMMERSIVE_RECENTS,
+                    Integer.parseInt((String) newValue), UserHandle.USER_CURRENT);
+            mImmersiveRecents.setValue((String) newValue);
+            mImmersiveRecents.setSummary(mImmersiveRecents.getEntry());
+
+            mPreferences = mContext.getSharedPreferences("recent_settings", Activity.MODE_PRIVATE);
+            if (!mPreferences.getBoolean("first_info_shown", false) && newValue != null) {
+                getActivity().getSharedPreferences("recent_settings", Activity.MODE_PRIVATE)
+                        .edit()
+                        .putBoolean("first_info_shown", true)
+                        .commit();
+                openAOSPFirstTimeWarning();
+            }
+            return true;
         }
         return false;
     }
@@ -136,6 +166,16 @@ public class Recents extends SettingsPreferenceFragment implements
             doOmniSwitchConfig();
         }
     }
+
+    private void openAOSPFirstTimeWarning() { 
+        new AlertDialog.Builder(getActivity()) 
+                .setTitle(getResources().getString(R.string.aosp_first_time_title)) 
+                .setMessage(getResources().getString(R.string.aosp_first_time_message)) 
+                .setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() { 
+                        public void onClick(DialogInterface dialog, int whichButton) { 
+                        } 
+                }).show(); 
+    } 
 
     private void doOmniSwitchConfig() {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
