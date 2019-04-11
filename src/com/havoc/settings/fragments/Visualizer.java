de2ca534a066322f7a29520497f09491d8ba9635
neobuddy89 <neobuddy89@gmail.com>
2019-04-11 23:35:03 +0530

    Lockscreen Visualizer: Add pulse magic [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/havoc/settings/fragments/Visualizer.java
+++ src/com/havoc/settings/fragments/Visualizer.java
@@ -19,10 +19,12 @@ package com.havoc.settings.fragments;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -32,21 +34,68 @@ import com.android.internal.logging.nano.MetricsProto;
 public class Visualizer extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
-    @Override
-    public void onCreate(Bundle savedInstance) {
-        super.onCreate(savedInstance);
-        addPreferencesFromResource(R.xml.visualizer_settings);
-        ContentResolver resolver = getActivity().getContentResolver();
-    }
+    private static final String KEY_AUTOCOLOR = "lockscreen_visualizer_autocolor";
+    private static final String KEY_LAVALAMP = "lockscreen_lavalamp_enabled";
+
+    private SwitchPreference mAutoColor;
+    private SwitchPreference mLavaLamp;
 
     @Override
-    public void onResume() {
-        super.onResume();
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.visualizer_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        boolean mMediaArtEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.LOCKSCREEN_MEDIA_METADATA, 1,
+                UserHandle.USER_CURRENT) != 0;
+        boolean mLavaLampEnabled = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.LOCKSCREEN_LAVALAMP_ENABLED, 1,
+                UserHandle.USER_CURRENT) != 0;
+
+        mAutoColor = (SwitchPreference) findPreference(KEY_AUTOCOLOR);
+        mAutoColor.setEnabled(mMediaArtEnabled && !mLavaLampEnabled);
+
+        if (!mMediaArtEnabled) {
+            mAutoColor.setSummary(getActivity().getString(
+                    R.string.lockscreen_autocolor_mediametadata));
+        } else if (mLavaLampEnabled) {
+            mAutoColor.setSummary(getActivity().getString(
+                    R.string.lockscreen_autocolor_lavalamp));
+        } else {
+            mAutoColor.setSummary(getActivity().getString(
+                    R.string.lockscreen_autocolor_summary));
+        }
+
+        mLavaLamp = (SwitchPreference) findPreference(KEY_LAVALAMP);
+        mLavaLamp.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLavaLamp) {
+            boolean mLavaLampEnabled = (Boolean) newValue;
+            boolean mMediaArtEnabled = Settings.System.getIntForUser(resolver,
+                Settings.System.LOCKSCREEN_MEDIA_METADATA, 1,
+                UserHandle.USER_CURRENT) != 0;
+
+            mAutoColor.setEnabled(mMediaArtEnabled && !mLavaLampEnabled);
+
+            if (!mMediaArtEnabled) {
+                mAutoColor.setSummary(getActivity().getString(
+                        R.string.lockscreen_autocolor_mediametadata));
+            } else if (mLavaLampEnabled) {
+                mAutoColor.setSummary(getActivity().getString(
+                        R.string.lockscreen_autocolor_lavalamp));
+            } else {
+                mAutoColor.setSummary(getActivity().getString(
+                        R.string.lockscreen_autocolor_summary));
+            }
+            return true;
+        }
         return false;
     }
 
